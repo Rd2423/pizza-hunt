57e52e4f3cf86c3fe1ec0b9f8682d00835048d4e
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-09-15 16:15:39 -0400

    api routes feature for commments and pizzas

--- models/Pizza.js
+++ models/Pizza.js
@@ -15,8 +15,27 @@ const PizzaSchema = new Schema({
       type: String,
       default: 'Large'
     },
+    comments: [
+      {
+        type: String.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ],
     toppings: []
-  });
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false
+  }
+);
+  
+
+  // get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
 
   // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
